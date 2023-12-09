ce4c412c646b3344058a8c970b2026d3fbc5e071
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-09 13:39:12 +0000

    Null, Undefined conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,20 +14,48 @@ let score = 33
 // output => number 
 
 
+// Note ============================= 
+// "33" => 33
+// "123abc" => NaN // Not a number (special type)
+// true => 1; false => 0
+
+
 
 // I want to convert this number value with the string.
 
-let someNumber = 12345
-let valueInString = String(someNumber)
-console.log (typeof valueInString)
+// let someNumber = 12345
+// let valueInString = String(someNumber)
+// console.log (typeof valueInString)
 
 
 // I want to convert this number value with the boolean.
 
-let mainNumber = 7676
-let valueInBoolean = Boolean(mainNumber)
-console.log (typeof valueInBoolean)
+// let mainNumber = 7676
+// let valueInBoolean = Boolean(mainNumber)
+// console.log (typeof valueInBoolean)
 
 
+// Note ===================================================
+// 1 => true; 0 => false
+// "" => false // value na thakle / empty hole false dekhabe
+// "Ahnaf" => true
+
+
+// let numtest = "9911abc"
+// let convertNumber = Number(numtest)
+// console.log (typeof convertNumber) //output => number
+// console.log (convertNumber) //output => NaN
+
+
+// let nullCheck = null
+// let convertNumberNull = Number(nullCheck)
+// console.log (typeof convertNumberNull) //output => number
+// console.log (convertNumberNull) //output => NaN
+
+
+let undefinedCheck = undefined
+let convertNumberUndefined = Number(undefinedCheck)
+// console.log (typeof convertNumberUndefined) //output => NaN
+console.log (convertNumberUndefined) //output => NaN
 
 
