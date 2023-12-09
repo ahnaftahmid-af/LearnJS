19932093f198be3cd9c5665837ab015af902cb8a
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-09 15:01:43 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,17 @@
 let score = 33
 
-// console.log(typeof score)
-// console.log(typeof (score))
+console.log(typeof score)
+console.log(typeof (score))
 
 
 // I want to convert this string value with the number.  
 
-//let age = "99" // front-end/user theke ata value neya hobe 
+let age = "99" // front-end/user theke ata value neya hobe 
 
-//let valueInNumber = Number(age) // sei value ta ke 'Number()' ar moddhe declear kore number a convert kore seti abr notun variable ar moddhe store kore rakha hoiche
+let valueInNumber = Number(age) // sei value ta ke 'Number()' ar moddhe declear kore number a convert kore seti abr notun variable ar moddhe store kore rakha hoiche
 
-//console.log(typeof valueInNumber) // and finally 'console.log' ar maddhome notun variable ta ke output dekha jabe 
-// output => number 
+console.log(typeof valueInNumber) // and finally 'console.log' ar maddhome notun variable ta ke output dekha jabe 
+output => number 
 
 
 // Note ============================= 
@@ -23,15 +23,15 @@ let score = 33
 
 // I want to convert this number value with the string.
 
-// let someNumber = 12345
-// let valueInString = String(someNumber)
+let someNumber = 12345
+let valueInString = String(someNumber)
 // console.log(typeof valueInString)
 
 
 // I want to convert this number value with the boolean.
 
-// let mainNumber = 7676
-// let valueInBoolean = Boolean(mainNumber)
+let mainNumber = 7676
+let valueInBoolean = Boolean(mainNumber)
 // console.log(typeof valueInBoolean)
 
 
@@ -41,14 +41,14 @@ let score = 33
 // "Ahnaf" => true
 
 
-// let numtest = "9911abc"
-// let convertNumber = Number(numtest)
+let numtest = "9911abc"
+let convertNumber = Number(numtest)
 // console.log(typeof convertNumber) //output => number
 // console.log(convertNumber) //output => NaN
 
 
-// let nullCheck = null
-// let convertNumberNull = Number(nullCheck)
+let nullCheck = null
+let convertNumberNull = Number(nullCheck)
 // console.log(typeof convertNumberNull) //output => number
 // console.log(convertNumberNull) //output => NaN
 
@@ -59,3 +59,42 @@ let convertNumberUndefined = Number(undefinedCheck)
 console.log(convertNumberUndefined) //output => NaN
 
 
+//Operation
+
+console.log(5 + 5)  //=> 10
+console.log(5 - 5)  //=> 0
+console.log(5 * 5)  //=> 25
+console.log(5 ** 5) //=> 3125
+console.log(5 / 5)  //=> 1
+console.log(5 % 5)  //=> 0
+
+
+let num1 = 20
+let num2 = 50
+let totalNumber = num1 + num2 
+console.log(totalNumber);   //=> 70
+
+
+// Increment
+
+let gameCounter = 100 
+gameCounter++; // Postfix increment
+console.log(gameCounter);   //=> 101
+
+let A = 200
+++A; // Prefix increment
+console.log(A)  //=> 201
+
+
+// Decrement
+
+let student_number = 33
+
+--student_number; // Prefix decrement
+
+console.log(student_number);    //=> 32
+
+
+let results = 80
+results--; // Postfix decrement
+console.log(results); //=> 79
