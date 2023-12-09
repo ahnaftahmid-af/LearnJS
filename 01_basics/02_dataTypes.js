a4160ac8e0bed976434338bc1fb5a7b9e91c169b
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-09 05:55:52 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+//alert( 2 + 2 ) // we are using NodeJs, not browser
+
+console.log (
+    3 + 
+    3
+)// code readabality should be high
+
+console.log ("Hello world")
+
+// Datatypes
+// number  => 123
+// string  => "Hello" / 'Hello'
+// bigint  => big data like facebook...
+// boolean => true / false
+// null    => empty / standalone value
+// undefiend => variable declear kora hoiche but akhono variable ar value assign (deya hoini) kora hoini
+// symbol  => unique korar jonno use kora hoi, react a besi use hoi
+// objet   
+
+console.log (typeof undefined) // output => Undefined
+console.log (typeof null) // output => Object
+console.log (typeof "Hello JS") // output => Hello JS 
+                                           //String
+
+// 'typeof' diye valuer datatype check kora hoi.
