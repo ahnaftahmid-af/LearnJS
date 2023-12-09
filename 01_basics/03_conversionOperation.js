d79da6dbdf7a39ed6efc91d69d92605211627257
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-09 09:09:21 +0000

    Variables Value Convert

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,33 @@
 let score = 33
 
-console.log (typeof score)
-console.log (typeof (score))
+// console.log (typeof score)
+// console.log (typeof (score))
+
+
+// I want to convert this string value with the number.  
+
+//let age = "99" // front-end/user theke ata value neya hobe 
+
+//let valueInNumber = Number(age) // sei value ta ke 'Number()' ar moddhe declear kore number a convert kore seti abr notun variable ar moddhe store kore rakha hoiche
+
+//console.log (typeof valueInNumber) // and finally 'console.log' ar maddhome notun variable ta ke output dekha jabe 
+// output => number 
+
+
+
+// I want to convert this number value with the string.
+
+let someNumber = 12345
+let valueInString = String(someNumber)
+console.log (typeof valueInString)
+
+
+// I want to convert this number value with the boolean.
+
+let mainNumber = 7676
+let valueInBoolean = Boolean(mainNumber)
+console.log (typeof valueInBoolean)
+
+
+
 
