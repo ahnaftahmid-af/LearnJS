9db3c19c80f035459b592866ec083cb8f22d55b0
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-10 04:34:40 +0000

    some new comments

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -57,3 +57,13 @@ const myFunc = function () {
   console.log("Hello World");
 };
 console.log(myFunc);
+
+//Type of val	Result
+// Undefined	| "undefined"
+// Null	      | "object"
+// Boolean	  | "boolean"
+// Number	    | "number"
+// String	    | "string"
+// Object (native and does not implement [[Call]])	"object"
+// Object (native or host and does implement [[Call]])	"function"
+// Object (host and does not implement [[Call]])	Implementation-defined except may not be "undefined", "boolean", "number", or "string".
