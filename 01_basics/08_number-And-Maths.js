5ceeef3a63f70480be469d2a4ca9b16be3fd005f
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-17 16:41:32 +0000

    Number and Maths

---
+++ 01_basics/08_number-And-Maths.js
@@ -0,0 +1,12 @@
+const score = 300;
+console.log(score);
+
+const mainNumber = new Number(400);
+console.log(mainNumber.toString().length);
+console.log(mainNumber.toFixed(2));
+
+const otherNumber = 327.489;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString());
