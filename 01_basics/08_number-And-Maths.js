7c0831414e0c7ea6626869e8ea2991e204a4ed9d
Ahnaf Tahmid <tahmidahnaf01@gmail.com>
2023-12-18 12:16:10 +0000

    Numbers and Maths

--- 01_basics/08_number-And-Maths.js
+++ 01_basics/08_number-And-Maths.js
@@ -10,3 +10,23 @@ console.log(otherNumber.toPrecision(4));
 
 const hundreds = 10000000;
 console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString("en-IN"));
+
+Maths;
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(7.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log(Math.min(7, 4, 2, 9, 5));
+console.log(Math.max(7, 4, 2, 9, 5));
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
